ce7ee2358bfbfc73d670d5f39e441706cfe41d66
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-24 23:21:15 +0200

    Add files via upload

---
+++ ReflectionBasics/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ReflectionBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialise t as typeof string 
+            Type t = typeof(string);
+
+            // Use Reflection to find about any sort of data related to t 
+            Console.WriteLine("Name : {0}", t.Name);
+            Console.WriteLine("Full Name : {0}", t.FullName);
+            Console.WriteLine("Namespace : {0}", t.Namespace);
+            Console.WriteLine("Base Type : {0}", t.BaseType);
+
+            foreach(var property in t.GetProperties())
+            {
+                Console.WriteLine(property.Name);
+            }
+
+            foreach(var field in t.GetFields())
+            {
+                Console.WriteLine($"{field.Name}");
+            }
+
+            foreach(var method in t.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
+}
